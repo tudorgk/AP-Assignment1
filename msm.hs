2e18c5269a83eaa811061af50917bad3388c9ae2
Tudor Dragan <tudorgk@gmail.com>
2014-09-13 12:01:01 +0200

    begin to implement custom errors

--- msm.hs
+++ msm.hs
@@ -2,14 +2,12 @@ module MSM where
 
 -- we want to use monads here
 import Control.Monad
+import Control.Applicative
 import Data.Maybe
 import Data.List as List
 
 -- and you might also find Maps useful
 import qualified Data.Map as Map
-
--- I want to debug stuff
-import Debug.Trace as Trace
 
 -- | The type of instructions for the MSM.
 data Inst 
@@ -61,68 +59,84 @@ data Error = Error { errorType :: ErrorType, errorMessage ::  String}
 initial :: Prog -> State
 initial p = State { prog = p
                   , pc = 0                     
-                  , stack = []                   
+                  , stack = []                 
                   , regs = Map.empty                      
                   }
 
 -- | This is the monad that is used to implement the MSM. 
-newtype MSM a = MSM (State ->Either ErrorType (a, State))
+newtype MSM a = MSM (State -> Either Error (a, State))
 
 instance Monad MSM where
     -- return :: a -> MSM a
-    return x = MSM (\s -> (x,s))
-    
-    -- (>>=) :: MSM a -> (a -> MSM b) -> MSM b
-    (MSM p) >>= f = MSM (\s -> let (aValue, previousState) = p s;
-                                           (MSM newStatefulComputation) = f aValue
-                                         in newStatefulComputation newState
-                            )
+    return a = MSM (\x -> Right (a,x))
     
     -- fail :: String -> MSM a
-    fail s = MSM (\x -> (x,s)) -- ??
+    fail s = MSM (\x -> Left Error{errorMessage = s, errorType = InvalidPC})
+    
+    -- (>>=) :: MSM a -> (a -> MSM b) -> MSM b
+    (MSM p) >>= k = MSM (\s -> case p s of
+                            Right v -> let Right (r, state1) = p s;
+                                               (MSM p1) = k r
+                                         in p1 state1
+                            Left v -> Left v
+                            )    
 
 ---- Remember to make your monad a functor as well
 instance Functor MSM where
   --fmap :: (Functor f) => (a -> b) -> f a -> f b  
-  f xs = xs >>= return . f
+  fmap f xs = xs >>= return . f
 
 ---- And perhaps also an Applicative
- instance Applicative MSM where
+instance Applicative MSM where
   pure = return
   df <*> dx = df >>= \f -> dx >>= return . f
 
 -- | `get` returns the current state of the running MSM.
 get :: MSM State
-get = MSM $ \s -> (_,s)  
+get = MSM (\x -> Right (x,x))
 
 -- | set a new state for the running MSM.
 set :: State -> MSM ()
-set newState = MSM $ \s -> ((),newState)  
+--set m| Trace.trace("called set state " ++ show m) False = undefined
+set m = MSM (\x -> Right ((),m))
 
----- | modify the state for the running MSM according to
----- the provided function argument
+-- | modify the state for the running MSM according to
+-- the provided function argument
 modify :: (State -> State) -> MSM ()
-modify f = MSM $ \s ->  ((), f s)
+modify f = MSM (\s -> Right ((), f s))
 
 
----- | This function provides the instruction the PC currently points
----- to. If the PC is out of bounds, the MSM halts with an error.
---getInst :: ...
---getInst = ...
+-- | This function provides the instruction the PC currently points
+-- to. If the PC is out of bounds, the MSM halts with an error.
+getInst :: MSM Inst
+getInst = do
+  stat <- get -- get the state
+  if pc stat > length (prog stat) || (pc stat) < 0 -- check pc from state bounds
+    then error "Invalid PC " -- respond with an error
+    else return $ (prog stat) !! (pc stat) -- return the inst at the PC index from the Prog list
+
+-- | This function runs the MSM.
+interp :: MSM ()
+interp = run
+    where run = do inst <- getInst
+                   cont <- interpInst inst
+                   when cont run
+
+-- | This function interprets the given instruction. It returns True
+-- if the MSM is supposed to continue it's execution after this
+-- instruction.
+interpInst :: Inst -> MSM Bool
+interpInst inst = do
+  currentState <- get
+  case inst of 
+    PUSH a -> do 
+        push a currentState
+        return True
 
 
----- | This function runs the MSM.
---interp :: MSM ()
---interp = run
---    where run = do inst <- getInst
---                   cont <- interpInst inst
---                   when cont run
+push :: Int -> State -> MSM ()
+push a currentState = set currentState{stack = a:stack currentState, pc = pc currentState +1} 
 
----- | This function interprets the given instruction. It returns True
----- if the MSM is supposed to continue it's execution after this
----- instruction.
---interpInst :: Inst -> MSM Bool
---interpInst inst = ...
 
 ---- | Run the given program on the MSM
 --runMSM :: Prog -> ...
